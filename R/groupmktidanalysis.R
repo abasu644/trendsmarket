92f169c0cda5c48cff7c1b7ed9e43af80d6c94c3
abasu <abasu644@gmail.com>
2014-08-02 20:08:47 +0530

    final

--- R/groupmktidanalysis.R
+++ R/groupmktidanalysis.R
@@ -1,5 +1,14 @@
 groupmktidanalysis <- function(mktid=-9999,rf_code="gfx")
 {
   rmddata=paste(system.file(package="trendsmarket"),"rmd/analysis.Rmd",sep="/")
-  knit2html(rmddata,output="groupmktidanalysis_temp.html",fragment.only = TRUE)
+  rmddata=paste (readLines(rmddata),collapse="\n")
+  finalfiledata=paste("```{r  warning=FALSE,error=TRUE,echo=F,message=FALSE,comment=NA} \n mktid=",mktid,
+                      " \n base_path='", 
+                      base_path, "' \n ",
+                      " ```",
+                      "\n",
+                      rmddata,
+                      sep="");
+  write(finalfiledata,file="groupmktidanalysis_temp.Rmd")
+  knit2html("groupmktidanalysis_temp.Rmd",output="groupmktidanalysis_temp.html",fragment.only = TRUE)
 }
